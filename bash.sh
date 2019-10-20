ad5c355585708a99598f8256eb71f7b32b05f458
Brad Garropy <bradgarropy@gmail.com>
2019-10-20 13:24:10 -0500

    Delete bash.sh

--- bash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl --output ~/.bash_profile https://raw.githubusercontent.com/bradgarropy/dotfiles/master/.bash_profile
-curl --output ~/.bashrc https://raw.githubusercontent.com/bradgarropy/dotfiles/master/.bashrc
